aa4da9b0c65f05216a9f0f5f1ec641e8103ca868
luctc <congluc19297@gmail.com>
2018-06-19 16:48:58 +0700

    Insert CSV to DB use goroutines

--- 05_Postgresql/04_ReadWriteCSVToDB/main.go
+++ 05_Postgresql/04_ReadWriteCSVToDB/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -74,11 +76,16 @@ func readCSVFile(fileName string, setComma rune) [][]string {
 
 func createTableQuote() {
 
+	// _, err := db.Exec(`CREATE TABLE IF NOT EXISTS quote (
+	// 	id 		SERIAL PRIMARY KEY NOT NULL,
+	// 	quote	TEXT NOT NULL,
+	// 	auth 	TEXT NOT NULL,
+	// 	kind 	TEXT
+	// )`)
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS quote (
-		id 		SERIAL PRIMARY KEY NOT NULL,
 		quote	TEXT NOT NULL,
-		auth 	TEXT NOT NULL,
-		kind 	TEXT
+		auth 	TEXT PRIMARY KEY NOT NULL,
+		kind 	TEXT NOT NULL
 	)`)
 
 	if err != nil {
@@ -87,16 +94,43 @@ func createTableQuote() {
 	fmt.Println("Successfully created table QUOTE!")
 }
 
-func insertDataToQuote(csvData [][]string) {
+func insertDataToQuote(csvData [][]string, fragNum int) {
 	lenData := len(csvData)
 	fmt.Println("Inserting...")
 	var i int
-	for i = 0; i < lenData; i++ {
+	var fragment int
+	start := time.Now()
+	fmt.Println(start)
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(fragNum)
+	fragment = lenData / fragNum
+	for i = 0; i < fragNum; i++ {
+		go func(idx int) {
+			defer wg.Done()
+			start := fragment * idx
+			end := fragment * (idx + 1)
+			if idx == (fragNum - 1) {
+				end = lenData
+			}
+			for j := start; j < end; j++ {
+				_, _ = db.Exec("INSERT INTO quote (QUOTE, AUTH, KIND) VALUES ($1, $2, $3)", csvData[j][0], csvData[j][1], csvData[j][2])
+			}
+
+		}(i)
+	}
+	// fmt.Println(fragment)
+	/*for i = 0; i < 100; i++ {
 		_, err := db.Exec("INSERT INTO quote (QUOTE, AUTH, KIND) VALUES ($1, $2, $3)", csvData[i][0], csvData[i][1], csvData[i][2])
 		if err != nil {
 			panic(err)
 		}
-	}
+	}*/
+	wg.Wait()
+	end := time.Now()
+	fmt.Println(end)
+
 	// for _, each := range csvData {
 	// 	_, err := db.Exec("INSERT INTO quote (QUOTE, AUTH, KIND) VALUES ($1, $2, $3)", each[0], each[1], each[2])
 
@@ -110,6 +144,26 @@ func main() {
 	csvData := readCSVFile("./quotes_all.csv", ';')
 
 	createTableQuote()
-	insertDataToQuote(csvData)
+	insertDataToQuote(csvData, 90)
+
+	/*50 goroutines
+	2018-06-19 14:32:28.539460984 +0700 +07 m=+0.135548243
+	2018-06-19 14:33:18.980734256 +0700 +07 m=+50.576821457*/
+
+	/*60 goroutines
+	2018-06-19 14:35:34.017387773 +0700 +07 m=+0.037819768
+	2018-06-19 14:36:10.819939037 +0700 +07 m=+36.840371200*/
+
+	/*70 goroutines
+	2018-06-19 14:36:40.21244279 +0700 +07 m=+0.040360130
+	2018-06-19 14:37:15.471842468 +0700 +07 m=+35.299759986*/
+
+	/*80 goroutines
+	2018-06-19 14:41:40.523446589 +0700 +07 m=+0.127882018
+	2018-06-19 14:42:07.619207725 +0700 +07 m=+27.223643151*/
+
+	/*90 goroutines
+	2018-06-19 14:43:33.279647591 +0700 +07 m=+0.038205097
+	2018-06-19 14:43:55.139216944 +0700 +07 m=+21.897774611*/
 	db.Close()
 }
